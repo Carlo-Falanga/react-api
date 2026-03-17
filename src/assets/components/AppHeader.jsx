1d4b21f6e3258ffe2dab410dae11981f44999d80
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-17 12:09:38 +0100

    complete milestone 1: make api data print in console

--- src/assets/components/AppHeader.jsx
+++ src/assets/components/AppHeader.jsx
@@ -1,3 +1,11 @@
 export default function AppHeader(){
 
+
+    return(
+        <header>
+            <section className="container">
+                <h1>Wonderful Cast</h1>
+            </section>
+        </header>
+    )
 }
--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+
+
 export default function AppMain(){
 
+    const url_api = 'https://lanciweb.github.io/demo/api/actresses/'
+
+    const [actresses, setActresses] = useState([])
+
+    function getActresses(){
+        fetch(url_api)
+        .then(res => res.json())
+        .then(data =>{
+            setActresses(data)
+        })
+    }
+
+
+    useEffect(getActresses,[])
+    console.log(actresses);
+    
+
+    return(
+    <main>
+
+    </main>
+    )
 }
 
