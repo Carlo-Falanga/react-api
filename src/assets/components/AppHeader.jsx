62f2c5d0c7d255c2bd8bb8054bc120a41de63726
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-17 13:50:02 +0100

    improve styling and add actor card section rendered with api

--- src/assets/components/AppHeader.jsx
+++ src/assets/components/AppHeader.jsx
@@ -2,7 +2,7 @@ export default function AppHeader() {
   return (
     <header>
       <section>
-        <div className="container">
+        <div className="container-fluid mb-5 mt-3 ms-3 fw-bolder">
           <h1>Wonderful Cast</h1>
         </div>
       </section>
--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,24 +1,38 @@
 import { useEffect, useState } from "react";
 
 export default function AppMain() {
-  const url_api = "https://lanciweb.github.io/demo/api/actresses/";
+  const url_actresses_api = "https://lanciweb.github.io/demo/api/actresses/";
+  const url_actors_api = "https://lanciweb.github.io/demo/api/actors/";
 
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
   function getActresses() {
-    fetch(url_api)
+    fetch(url_actresses_api)
       .then((res) => res.json())
       .then((data) => {
         setActresses(data);
       });
   }
 
+  function getActors() {
+    fetch(url_actors_api)
+      .then((res) => res.json())
+      .then((data) => {
+        setActors(data);
+      });
+  }
+
   useEffect(getActresses, []);
+  useEffect(getActors, []);
 
   return (
     <main>
-      <section>
+      <section className="actresses_cards my-5 py-5">
         <div className="container">
+          <h2 className="text-bg-dark d-inline-block mb-3 px-4 py-2 rounded-2">
+            Actresses
+          </h2>
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
             {actresses.map((actress) => (
               <div className="col" key={actress.id}>
@@ -26,11 +40,46 @@ export default function AppMain() {
                   <img src={actress.image} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">{actress.name}</h5>
-                    <p className="card-text"><small className="text-body-secondary">{actress.birth_year}, {actress.nationality}</small></p>
-                    <p className="card-text fst-italic">
-                      {actress.biography}
+                    <p className="card-text">
+                      <small className="text-body-secondary">
+                        {actress.birth_year}, {actress.nationality}
+                      </small>
                     </p>
-                    <p className="card-text text-start-bottom"><span className="fw-medium">Best Movies:</span> <br/> {actress.most_famous_movies}</p>
+                    <p className="card-text fst-italic">{actress.biography}</p>
+                    <p className="card-text text-start-bottom">
+                      <span className="fw-medium">Best Movies:</span> <br />{" "}
+                      {actress.most_famous_movies}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="actors_cards">
+        <div className="container">
+          <h2 className="text-bg-dark d-inline-block mb-3 px-4 py-2 rounded-2">
+            Actors
+          </h2>
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            {actors.map((actor) => (
+              <div className="col" key={actor.id}>
+                <div className="card h-100">
+                  <img src={actor.image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{actor.name}</h5>
+                    <p className="card-text">
+                      <small className="text-body-secondary">
+                        {actor.birth_year}, {actor.nationality}
+                      </small>
+                    </p>
+                    <p className="card-text fst-italic">{actor.biography}</p>
+                    <p className="card-text text-start-bottom">
+                      <span className="fw-medium">Best Movies:</span> <br />{" "}
+                      {actor.known_for}
+                    </p>
                   </div>
                 </div>
               </div>
