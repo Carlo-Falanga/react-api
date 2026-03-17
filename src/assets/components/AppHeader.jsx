bef5c28e05a7bcd750ba71a4a8b1b3e2d728baa4
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-17 12:38:41 +0100

    complete milestone 2: add actresses cards with name, bio, image and details

--- src/assets/components/AppHeader.jsx
+++ src/assets/components/AppHeader.jsx
@@ -1,11 +1,11 @@
-export default function AppHeader(){
-
-
-    return(
-        <header>
-            <section className="container">
-                <h1>Wonderful Cast</h1>
-            </section>
-        </header>
-    )
+export default function AppHeader() {
+  return (
+    <header>
+      <section>
+        <div className="container">
+          <h1>Wonderful Cast</h1>
+        </div>
+      </section>
+    </header>
+  );
 }
--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,29 +1,43 @@
 import { useEffect, useState } from "react";
 
+export default function AppMain() {
+  const url_api = "https://lanciweb.github.io/demo/api/actresses/";
 
-export default function AppMain(){
+  const [actresses, setActresses] = useState([]);
 
-    const url_api = 'https://lanciweb.github.io/demo/api/actresses/'
+  function getActresses() {
+    fetch(url_api)
+      .then((res) => res.json())
+      .then((data) => {
+        setActresses(data);
+      });
+  }
 
-    const [actresses, setActresses] = useState([])
+  useEffect(getActresses, []);
 
-    function getActresses(){
-        fetch(url_api)
-        .then(res => res.json())
-        .then(data =>{
-            setActresses(data)
-        })
-    }
-
-
-    useEffect(getActresses,[])
-    console.log(actresses);
-    
-
-    return(
+  return (
     <main>
-
+      <section>
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 ">
+            {actresses.map((actress) => (
+              <div className="col" key={actress.id}>
+                <div className="card">
+                  <img src={actress.image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{actress.name}</h5>
+                    <p className="card-text"><small className="text-body-secondary">{actress.birth_year} {actress.nationality}</small></p>
+                    <p className="card-text">
+                      {actress.biography}
+                    </p>
+                    <p className="card-text">Best Movies: <br/> {actress.most_famous_movies}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </main>
-    )
+  );
 }
-
